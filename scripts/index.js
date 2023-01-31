c649cc64dde549f0c8c85263ebfce85d3df7171c
Chingis Tsybikov <fendral@yandex.ru>
2023-01-31 20:11:27 +0600

    Added function to copy place info, fixed content block width

--- scripts/index.js
+++ scripts/index.js
@@ -13,33 +13,27 @@ const placeForm = popupPlace.querySelector('.popup__form');
 const initialPlaces = [
   {
     name: 'Эль-Капитан',
-    link: './images/image-1.jpg',
-    alt: 'Деревья и поляна перед горой Эль-Капитан, Калифорния'
+    link: './images/image-1.jpg'
   },
   {
     name: 'Усти-над-Лабем',
-    link: './images/image-2.jpg',
-    alt: 'Яркое солнце на лесом с холмами рядом с городом Усти-над-Лабем, Чехия'
+    link: './images/image-2.jpg'
   },
   {
     name: 'Озеро Пейто',
-    link: './images/image-3.jpg',
-    alt: 'Озеро Пейто, лазурного цвета под горой в лесной местности, Канада'
+    link: './images/image-3.jpg'
   },
   {
     name: 'Водопад Фаллох',
-    link: './images/image-4.jpg',
-    alt: 'Невысокий водопад Фаллох, немного деревьев и небо на фоне, Шотландия'
+    link: './images/image-4.jpg'
   },
   {
     name: 'Озеро Брайес',
-    link: './images/image-5.jpg',
-    alt: 'Вид из окна на озеро Брайес с лодками, Италия'
+    link: './images/image-5.jpg'
   },
   {
     name: 'Холмы Нанди',
-    link: './images/image-6.jpg',
-    alt: 'Прямая мощёная дорога в лесу со ступеньками, холмы Нанди, Индия'
+    link: './images/image-6.jpg'
   }
 ];
 let profileNameInput = profileForm.querySelector('[name="profileName"]');
@@ -62,30 +56,32 @@ function handleProfileFormSubmit (evt) {
   togglePopupWindow(popupProfile);
 }
 
+function copyPlaceNameAndLink (placeName, placeLink) {
+  const newPlaceDeepCopy = templateElement.cloneNode(true);
+  newPlaceDeepCopy.querySelector('.place__name').textContent = placeName;
+  newPlaceDeepCopy.querySelector('.place__image').src = placeLink;
+  return newPlaceDeepCopy;
+}
+
+function addInitialPlaces () {
+  initialPlaces.forEach((item) => {
+    const newPlace = copyPlaceNameAndLink(item.name, item.link);
+    placesElement.append(newPlace);
+  });
+}
+
+function addNewPlace(name, link) {
+  const newPlace = copyPlaceNameAndLink(name, link);
+  placesElement.prepend(newPlace);
+}
+
 function handlePlaceFormSubmit (evt) {
   evt.preventDefault();  
   addNewPlace(placeNameInput.value, placeImageSrcInput.value);
   togglePopupWindow(popupPlace);
 }
 
-function addPlacesInitial () {
-  initialPlaces.forEach((item) => {
-    const newPlace = templateElement.cloneNode(true);
-    newPlace.querySelector('.place__name').textContent = item.name;
-    newPlace.querySelector('.place__image').src = item.link;
-    newPlace.querySelector('.place__image').alt = item.alt;
-    placesElement.append(newPlace);
-  });
-}
-
-function addNewPlace(name, link) {
-  const newPlace = templateElement.cloneNode(true);
-  newPlace.querySelector('.place__name').textContent = name;
-  newPlace.querySelector('.place__image').src = link;
-  placesElement.prepend(newPlace);
-}
-
-addPlacesInitial();
+addInitialPlaces();
 
 /*окрашивание лайков и удаление карточек*/
 placesElement.addEventListener('click', function(event) {
